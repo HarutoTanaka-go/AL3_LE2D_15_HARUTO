37ea433ce76a1ceaf0855572f61bedf35a7d9cea
HarutoTanaka-go <k024g0048@g.neec.ac.jp>
2025-04-14 17:02:11 +0900

    コードを変更しました

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -2,8 +2,35 @@
 
 using namespace KamataEngine;
 
-void GameScene::Initialize() {}
+void GameScene::Initialize() {
+
+	textureHandle_ = TextureManager::Load("mario.jpg");
+
+	// 3Dモデルの生成
+	model_ = Model::Create();
+
+	delete model_;
+
+	// ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+
+	// カメラの初期化
+	camera_.Initialize();
+}
 
 void GameScene::Update() {}
 
-void GameScene::Draw() {}
+void GameScene::Draw() {
+
+	// irectXCommonのインスタンスの取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+	// 3Dモデル描画前処理
+	Model::PreDraw(dxCommon->GetCommandList());
+
+	// 3Dモデル描画
+	model_->Draw(worldTransform_, camera_, textureHandle_);
+
+	// 3Dモデル描画後処理
+	Model::PostDraw();
+}
--- DirectXGame/GameScene.h
+++ DirectXGame/GameScene.h
@@ -1,16 +1,30 @@
 #pragma once
-#include"KamataEngine.h"
+#include "KamataEngine.h"
 
 class GameScene {
-	public:
-		//初期化
-	    void Initialize();
+public:
+	// 初期化
+	void Initialize();
 
-		//更新
-	    void Update();
+	// 更新
+	void Update();
 
-		//描画
-	    void Draw();
+	// 描画
+	void Draw();
+
+	// テクスチャハンドル
+	uint32_t textureHandle_ = 0;
+
+	// 3Dモデル
+	KamataEngine::Model* model_ = nullptr;
+
+	//ワールドトランスフォーム
+	KamataEngine::WorldTransform worldTransform_;
+
+
+	//カメラ
+	KamataEngine::Camera camera_;
+
 
 
 };
